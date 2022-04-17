114353b5e4198a650d38e2d6f42d88c6e1d2c5d7
littlep1 <382399691@qq.com>
2022-04-17 17:15:01 +0800

    init

---
+++ src/main/java/com/zzm/community/dao/DiscussPostMapper.java
@@ -0,0 +1,16 @@
+package com.zzm.community.dao;
+
+import com.zzm.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset,int limit);
+
+    // 用于给参数去别名，如果方法只有一个参数，并且在<if>里使用，就必须加别名
+    int selectDiscussPostRows(@Param("userId")int userId);
+}
