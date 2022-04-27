a44a3ae48e5e1966c0f3093fe72755f024b997ed
littlep1 <382399691@qq.com>
2022-04-27 16:20:07 +0800

    add discuss post detail

--- src/main/java/com/zzm/community/dao/controller/DiscussPostController.java
+++ src/main/java/com/zzm/community/dao/controller/DiscussPostController.java
@@ -4,10 +4,13 @@ import com.zzm.community.annotation.LoginRequired;
 import com.zzm.community.entity.DiscussPost;
 import com.zzm.community.entity.User;
 import com.zzm.community.service.DiscussPostService;
+import com.zzm.community.service.UserService;
 import com.zzm.community.util.CommunityUtil;
 import com.zzm.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +26,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -43,6 +49,16 @@ public class DiscussPostController {
         System.out.println(insertSuccess);
         // 报错的情况，将来统一处理
         return CommunityUtil.getJSONString(0, "发布成功!");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussionPostId, Model model) {
+        DiscussPost post = discussPostService.findDiscussPostById(discussionPostId);
+        model.addAttribute("post", post);
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user",user);
+
+        return "/site/discuss-detail";
     }
 
 }
